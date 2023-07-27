7b956d3ee1c98f8ef26975c511abb6b5141ca594
superlopez-real <kakskemail@gmail.com>
2023-07-27 19:06:57 +0100

    sidebar fix

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ function createWindow() {
     width: mainWindowState.width,
     height: mainWindowState.height,
     titleBarStyle: "hidden",
-    trafficLightPosition: { x: 16, y: 24 },
+    trafficLightPosition: { x: 74, y: 24 },
     webPreferences: {
       nodeIntegrationInSubFrames: true,
       affinity: "main-window",
@@ -86,7 +86,7 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", function() {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -96,6 +96,6 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
